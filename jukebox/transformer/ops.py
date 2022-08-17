0196132c42161083fb90d662210b217ef0786671
coltraneboogie69 <111308797+coltraneboogie69@users.noreply.github.com>
2022-08-17 17:33:01 -0400

    Update ops.py

--- jukebox/transformer/ops.py
+++ jukebox/transformer/ops.py
@@ -80,6 +80,113 @@ def _convert_embedding_weights_to_fp32(l):
     if isinstance(l, t.nn.Embedding):
         l.weight.data = l.weight.data.float()
 
+def discretized_mix_logistic_loss(y_hat, y, num_classes=256,
+                                  log_scale_min=-7.0, reduce=True):
+    """Discretized mixture of logistic distributions loss
+    Note that it is assumed that input is scaled to [-1, 1].
+    Args:
+        y_hat (Tensor): Predicted output (B x C x T)
+        y (Tensor): Target (B x T x 1).
+        num_classes (int): Number of classes
+        log_scale_min (float): Log scale minimum value
+        reduce (bool): If True, the losses are averaged or summed for each
+          minibatch.
+    Returns
+        Tensor: loss
+    """
+    assert y_hat.dim() == 3
+    assert y_hat.size(1) % 3 == 0
+    nr_mix = y_hat.size(1) // 3
+
+    # (B x T x C)
+    y_hat = y_hat.transpose(1, 2)
+
+    # unpack parameters. (B, T, num_mixtures) x 3
+    logit_probs = y_hat[:, :, :nr_mix]
+    means = y_hat[:, :, nr_mix:2 * nr_mix]
+    log_scales = torch.clamp(y_hat[:, :, 2 * nr_mix:3 * nr_mix], min=log_scale_min)
+
+    # B x T x 1 -> B x T x num_mixtures
+    y = y.expand_as(means)
+
+    centered_y = y - means
+    inv_stdv = torch.exp(-log_scales)
+    plus_in = inv_stdv * (centered_y + 1. / (num_classes - 1))
+    cdf_plus = torch.sigmoid(plus_in)
+    min_in = inv_stdv * (centered_y - 1. / (num_classes - 1))
+    cdf_min = torch.sigmoid(min_in)
+
+    # log probability for edge case of 0 (before scaling)
+    # equivalent: torch.log(torch.sigmoid(plus_in))
+    log_cdf_plus = plus_in - F.softplus(plus_in)
+
+    # log probability for edge case of 255 (before scaling)
+    # equivalent: (1 - torch.sigmoid(min_in)).log()
+    log_one_minus_cdf_min = -F.softplus(min_in)
+
+    # probability for all other cases
+    cdf_delta = cdf_plus - cdf_min
+
+    mid_in = inv_stdv * centered_y
+    # log probability in the center of the bin, to be used in extreme cases
+    # (not actually used in our code)
+    log_pdf_mid = mid_in - log_scales - 2. * F.softplus(mid_in)
+
+    # tf equivalent
+    """
+    log_probs = tf.where(x < -0.999, log_cdf_plus,
+                         tf.where(x > 0.999, log_one_minus_cdf_min,
+                                  tf.where(cdf_delta > 1e-5,
+                                           tf.log(tf.maximum(cdf_delta, 1e-12)),
+                                           log_pdf_mid - np.log(127.5))))
+    """
+    # TODO: cdf_delta <= 1e-5 actually can happen. How can we choose the value
+    # for num_classes=65536 case? 1e-7? not sure..
+    inner_inner_cond = (cdf_delta > 1e-5).float()
+
+    inner_inner_out = inner_inner_cond * \
+        torch.log(torch.clamp(cdf_delta, min=1e-12)) + \
+        (1. - inner_inner_cond) * (log_pdf_mid - np.log((num_classes - 1) / 2))
+    inner_cond = (y > 0.999).float()
+    inner_out = inner_cond * log_one_minus_cdf_min + (1. - inner_cond) * inner_inner_out
+    cond = (y < -0.999).float()
+    log_probs = cond * log_cdf_plus + (1. - cond) * inner_out
+
+    log_probs = log_probs + F.log_softmax(logit_probs, -1)
+
+    if reduce:
+        return -torch.mean(log_sum_exp(log_probs))
+    else:
+        return -log_sum_exp(log_probs).unsqueeze(-1)
+
+def sample_mol(y_hat, num_classes=256):
+    """Sample from mixture of logistics.
+    y_hat: NxC where C is 3*number of logistics
+    """
+    assert len(y_hat.shape) == 2
+
+    N = y_hat.size(0)
+    nr_mix = y_hat.size(1) // 3
+
+    probs = torch.softmax(y_hat[:, :nr_mix], dim=-1)
+    means = y_hat[:, nr_mix:2 * nr_mix]
+    scales = torch.nn.functional.elu(y_hat[:, 2*nr_mix:3*nr_mix]) + 1.0001
+
+    indices = torch.multinomial(probs, 1).squeeze()
+    batch_indices = torch.arange(N, device=probs.device)
+    mu = means[batch_indices, indices]
+    s = scales[batch_indices, indices]
+    u = torch.rand(N, device=probs.device)
+    preds = mu + s*(torch.log(u) - torch.log(1-u))
+
+    return torch.min(
+        torch.max(
+            torch.round((preds+1)/2*(num_classes-1)),
+            preds.new_zeros(1),
+        ),
+        preds.new_ones(1)*(num_classes-1)
+    ).long().view(N, 1)
+        
 class Conv1D(nn.Module):
     def __init__(self, n_in, n_out, zero_out=False, init_scale=1.0):
         super(Conv1D, self).__init__()
